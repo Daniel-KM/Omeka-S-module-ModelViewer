01cf012eb135a00702f64eca63ae52054b90eb70
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-12-06 00:00:00 +0000

    Checked model attributes.

--- src/Media/FileRenderer/Model.php
+++ src/Media/FileRenderer/Model.php
@@ -17,8 +17,12 @@ class Model implements RendererInterface
      * @var array
      */
     protected $defaultOptions = [
-        'attributes' => 'class="model" allowfullscreen="allowfullscreen" style="height: 600px; height: 70vh;"',
         'template' => self::PARTIAL_NAME,
+        'attributes' => [
+            'class' => 'model-viewer',
+            'allowfullscreen' => 'allowfullscreen',
+            'style' => 'height: 600px; height: 70vh; width: 100%; border: none;',
+        ],
     ];
 
     /**
@@ -29,26 +33,53 @@ class Model implements RendererInterface
      * @param PhpRenderer $view,
      * @param MediaRepresentation $media
      * @param array $options These options are managed for sites:
-     *   - template: the partial to use
-     *   - attributes: set the attributes to add
+     *   - template: the partial to use.
+     *   - attributes: set the attributes of the iframe as a array; the default
+     *     class "model-viewer" is added and default height and width too (style).
      *   - config: config of the viewer, overriding resource and admin settings.
      * @return string
      */
     public function render(PhpRenderer $view, MediaRepresentation $media, array $options = [])
     {
+        $plugins = $view->getHelperPluginManager();
+
         $status = $view->status();
         if ($status->isSiteRequest()) {
             $template = $options['template'] ?? $this->defaultOptions['template'];
-            $options['attributes'] = $options['attributes'] ?? $this->defaultOptions['attributes'];
+            if (empty($options['attributes'])) {
+                $options['attributes'] = $this->defaultOptions['attributes'];
+            } else {
+                // Check attributes for security.
+                $escapeAttr = $plugins->get('escapeHtmlAttr');
+                foreach ($options['attributes'] as $name => $value) {
+                    if ($value === false || preg_match('/[^\w:.-]/', $name)) {
+                        unset($options['attributes'][$name]);
+                    } elseif ($value === true) {
+                        $options['attributes'][$name] = $name;
+                    } else {
+                        $options['attributes'][$name] = $escapeAttr($value);
+                    }
+                }
+                if (empty($options['attributes']['class'])) {
+                    $options['attributes']['class'] = 'model-viewer';
+                } elseif (strpos($options['attributes']['class'], 'model-viewer') === false) {
+                    $options['attributes']['class'] = 'model-viewer ' . $options['attributes']['class'];
+                }
+            }
         } else {
             $template = $this->defaultOptions['template'];
             $options['attributes'] = $this->defaultOptions['attributes'];
         }
 
-        unset($options['template']);
-        return $view->partial($template, [
+        $vars = [
             'resource' => $media,
             'options' => $options,
-        ]);
+        ];
+
+        unset($options['template']);
+        $partial = $plugins->get('partial');
+        return $template !== self::PARTIAL_NAME && $view->resolver($template)
+            ? $partial($template, $vars)
+            : $partial(self::PARTIAL_NAME, $vars);
     }
 }
