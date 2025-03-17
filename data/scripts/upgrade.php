38dbcfcd6a6d12a035f1379c7f360baa64d73e91
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-17 00:00:00 +0000

    Released version 3.4.2-174.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,6 +2,8 @@
 
 namespace ModelViewer;
 
+use Common\Stdlib\PsrMessage;
+
 /**
  * @var Module $this
  * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
@@ -26,10 +28,31 @@ $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
 $entityManager = $services->get('Omeka\EntityManager');
 
+$localConfig = require dirname(__DIR__, 2) . '/config/module.config.php';
+
 if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.66')) {
     $message = new \Omeka\Stdlib\Message(
         $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
         'Common', '3.4.66'
     );
     throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+}
+if (version_compare($oldVersion, '3.4.2', '<')) {
+    $this->updateWhitelist();
+
+    $json = $settings->get('modelviewer_config_default');
+    $json = $json ? json_decode($json, true) : null;
+    if (!$json) {
+        $settings->set('modelviewer_config_default', $localConfig['modelviewer']['settings']['modelviewer_config_default']);
+    }
+
+    $message = new PsrMessage(
+        'The version of ThreeJs was upgraded from 150 to 174. Check your models to see how they display. If needed, a new option allows to load all ThreeJS addons.' // @translate
+    );
+    $messenger->addWarning($message);
+
+    $message = new PsrMessage(
+        'The support of gltf v1 is deprecated and removed. Upgrade your old models to gltf v2.' // @translate
+    );
+    $messenger->addWarning($message);
 }
